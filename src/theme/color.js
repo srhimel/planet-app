1404bdee96610cf06bfaeef1b38306536d02c61c
srhimel <himusr@gmail.com>
2022-05-20 23:52:05 +0600

    folder structure

---
+++ src/theme/color.js
@@ -0,0 +1,14 @@
+export const colors = {
+  black: "#070724",
+  white: "#ffffff",
+  darkGrey: "#38384F",
+  grey: "#838391",
+  lightGreen: "#419EBB",
+  orange: "#EDA249",
+  purple: "#6f2ed6",
+  darkOrange: "#D14C32",
+  red: "#D83A34",
+  lightRed: "#CD5120",
+  green: "#1EC2A4",
+  blue: "#2D68F0"
+}
