0f1404a1c9a3bbb1699c5b60f22ef825f19d3d3a
srhimel <himusr@gmail.com>
2022-05-21 09:27:36 +0600

    added font

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { useFonts } from 'expo-font'
 
 export default function App() {
+  const [loaded] = useFonts({
+    AntonioMedium: require('./assets/fonts/Antonio-Medium.ttf'),
+    SpartanBold: require('./assets/fonts/Spartan-Bold.ttf'),
+    SpartanRegular: require('./assets/fonts/Spartan-Regular.ttf')
+  });
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +23,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
