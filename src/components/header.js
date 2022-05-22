f778626f42cd5171c11858f46bbb1ce4a051d2f2
srhimel <himusr@gmail.com>
2022-05-22 23:47:29 +0600

    fixed the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,13 @@
-import { View, StyleSheet, Pressable } from "react-native"
+import { View, StyleSheet, Pressable, NativeModules, Platform } from "react-native"
 import { colors } from "../theme/colors"
 import { spacing } from "../theme/spacing"
 import { Text } from "./text/text"
 import { AntDesign } from '@expo/vector-icons';
 import { useNavigation } from "@react-navigation/native";
+const { StatusBarManager } = NativeModules;
 
 
-export default Header = ({ backBtn, title = "The Planets" }) => {
+const Header = ({ backBtn, title = "The Planets" }) => {
   const navigation = useNavigation()
   return (
     <View style={styles.container}>
@@ -23,13 +24,17 @@ export default Header = ({ backBtn, title = "The Planets" }) => {
 
 }
 
-
 const styles = StyleSheet.create({
   container: {
     padding: spacing[4],
     borderBottomWidth: .5,
     borderBottomColor: colors.white,
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
+    marginTop: Platform.OS === 'android' ? StatusBarManager.HEIGHT : 0
   }
 })
+
+export default Header
+
+
